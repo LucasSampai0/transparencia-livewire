6bce8f80eeb1789d711af08e4e00a1a4c162d19f
Lucas Sampaio Bueno <lsampaiobueno@gmail.com>
2024-06-20 16:58:58 -0300

    .

--- resources/views/livewire/form-client.blade.php
+++ resources/views/livewire/form-client.blade.php
@@ -3,7 +3,7 @@
         <div class="relative z-0 w-full mb-5 group">
             <div class="relative z-0 w-full mb-5 group">
                 <label class="block mb-2 text-sm font-medium text-gray-500 transform scale-75 top-3 -z-10 origin-[0] dark:text-white" for="file_input">Foto do Cliente</label>
-                <input class="block w-full text-sm text-gray-500 border  border-gray-300  rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
+                <input class="block w-full text-sm text-gray-500 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
             </div>
         </div>
         <div class="relative z-0 w-full mb-5 group">
@@ -30,6 +30,6 @@
                 </div>
             </div>
         </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Salvar Cliente</button>
     </form>
 </div>
